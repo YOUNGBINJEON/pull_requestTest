34a8ac3c02da3be76ff3451edb4a05b13abafe7b
YOUNGBINJEON <sh44972@gmail.com>
2021-04-27 14:45:55 +0900

    [Create] First commit

---
+++ githubtest-04/src/main/java/com/multi/githubtest04/Githubtest04Application.java
@@ -0,0 +1,13 @@
+package com.multi.githubtest04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Githubtest04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Githubtest04Application.class, args);
+	}
+
+}
---
+++ githubtest-04/src/test/java/com/multi/githubtest04/Githubtest04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.githubtest04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Githubtest04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
